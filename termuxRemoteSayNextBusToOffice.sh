ce103fc45f37b4f9bae63e81d7f1c67f1d928955
Liang Liu <nlprliu@gmail.com>
2016-04-17 11:24:51 -0400

    Only speak the prediction of next bus when the prediction is within 4~10 minutes.

--- termuxRemoteSayNextBusToOffice.sh
+++ termuxRemoteSayNextBusToOffice.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 nextBusToOffice=$($HOME/dev/NextBus/3T.sh -e)
-[ -z "$nextBusToOffice" ] && nextBusToOffice="No current prediction"
-echo "$nextBusToOffice" | "$DIR/termuxRemoteSay.sh"
+[ -z "$nextBusToOffice" ] && { echo "No current prediction" | "$DIR/termuxRemoteSay.sh"; exit 0; }
 
+_say_if_in_range() {
+    [[ "$1" -le 10 && "$1" -ge 4 ]] && { echo "$1 minutes" | "$DIR/termuxRemoteSay.sh"; exit 0; }
+}
+
+first=$(echo "$nextBusToOffice" | awk '{print $1}')
+_say_if_in_range $first
+
+second=$(echo "$nextBusToOffice" | awk '{print $2}')
+[ -z "$second" ] || _say_if_in_range $second

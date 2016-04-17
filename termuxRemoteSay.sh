972153e3eb25ffd38772e4e3694c23bbe91ef0dc
Liang Liu <nlprliu@gmail.com>
2016-04-17 10:50:58 -0400

    Added some scripts to trigger termux-tts-speak on a remote Android device.

---
+++ termuxRemoteSay.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+config_file="$DIR/.config"
+[ -e "$config_file" ] || { echo "Cannot find .config file."; exit 1; }
+source "$config_file"
+
+ssh -p $remote_port $remote_host "termuxSay $1"
